cbfdf92613820089ab0100def5d6a623d4b9bf27
zzy <765205884@qq.com>
2021-08-29 15:47:33 +0800

    swiper works

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const links = document.querySelector(".links")
 ham.addEventListener("click", () => {
   links.classList.toggle("show")
 })
+
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  loop: true,
+})
